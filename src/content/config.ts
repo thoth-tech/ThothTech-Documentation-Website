9066ce50de12883ec513a7513b27688bbd314836
Quinn Curtis <xcurtis@deakin.edu.au>
2023-11-24 14:39:46 +1100

    Created full blog integration with tutorials
    this combines three of my trello cards:
    - Create blog
    - Written tutorial
    - Video tutorial

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,7 +1,10 @@
-import { defineCollection } from 'astro:content';
-import { docsSchema, i18nSchema } from '@astrojs/starlight/schema';
+import { defineCollection } from "astro:content";
+import { docsSchema, i18nSchema } from "@astrojs/starlight/schema";
+import { docsAndBlogSchema } from "starlight-blog/schema";
 
 export const collections = {
-	docs: defineCollection({ schema: docsSchema() }),
-	i18n: defineCollection({ type: 'data', schema: i18nSchema() }),
+  //og schema replaced with blog support one
+  //docs: defineCollection({ schema: docsSchema() }),
+  docs: defineCollection({ schema: docsAndBlogSchema }),
+  i18n: defineCollection({ type: "data", schema: i18nSchema() }),
 };
